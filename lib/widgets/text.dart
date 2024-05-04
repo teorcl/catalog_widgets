e6af0911ee9cd4829dfcc68eaf9614ed25a425ce
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2024-05-04 02:10:08 -0500

    no message

---
+++ lib/widgets/text.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomTextWidget extends StatelessWidget {
+  final String text;
+
+  const CustomTextWidget({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
